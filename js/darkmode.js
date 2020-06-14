44289792c2c5e57c49aaf420bab4f9cfec053637
Arjun Gandhi <arjungandhi06@gmail.com>
2020-06-14 03:24:46 -0400

    listen bitch i spent 3 hours on my life just for a stupid joke of having unreasonable large buttons and they fucked up my mobile site and my will to live hide the button hide your kids hide your wife and jesus christ i want death upan the word button.

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,23 +3,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
     button=document.getElementById("darkmode");
     button.onclick=function(){
         cur_theme=document.documentElement.getAttribute('data-theme')
-            b=document.getElementById("darkmode");
-            i=b.children[0];
 
             if (cur_theme=='dark') {
                 document.documentElement.setAttribute('data-theme', 'light');
-                b.classList.remove('light')
-                b.classList.add('dark')
-                i.classList.remove('fa-sun')
-                i.classList.add('fa-moon')
                 
             }
             else {
                 document.documentElement.setAttribute('data-theme', 'dark');
-                b.classList.remove('dark')
-                b.classList.add('light')
-                i.classList.remove('fa-moon')
-                i.classList.add('fa-sun')
             }    
         }
     })
